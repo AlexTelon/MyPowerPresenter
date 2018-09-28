c5868af39a262ffece6336f9dad61749b869a8ba
Alex Telon <alex.telon@gmail.com>
2018-09-28 20:15:02 +0200

    Initial black background presenter screen

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomPresenter.Utils;
+
+namespace CustomPresenter
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public string HelloWorld
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public ObservableCollection<string> CollectionOfWords { get; set; } = new ObservableCollection<string>() { "a", "few", "words" };
+
+        public MainWindowViewModel()
+        {
+            HelloWorld = "hello world test string";
+        }
+    }
+}

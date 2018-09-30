39aab08ee40cbb21e5e88a12622523e51b7a616f
Alex Telon <alex.telon@gmail.com>
2018-09-30 13:48:09 +0200

    Added load and create presentations from file

---
+++ PresenterCore/LoadPresentation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresenterCore
+{
+    /// <summary>
+    /// Load presentations from files.
+    /// </summary>
+    public class LoadPresentation
+    {
+        public static string[] ValidFileEndings => new string[] { "md" };
+
+        public static Presentation LoadFromFile(string file)
+        {
+            var contents = File.ReadAllLines(file);
+
+            return CreatePresentationFromFileContents(contents);
+        }
+
+        private static bool IsTitle(string candidate) => candidate[0] == '#';
+
+
+        public static Presentation CreatePresentationFromFileContents(IEnumerable<string> contents)
+        {
+            var presentation = new Presentation();
+
+            var currentSlide = new Slide();
+
+            List<string> data = contents.Select(x => x.Trim()).Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+            for (int i = 0; i < data.Count(); i++)
+            {
+                var line = data[i];
+
+                if (IsTitle(line))
+                {
+                    // take the title and everything up until the next title
+                    var dataUntilNextSlide = data
+                        .Skip(i)
+                        .TakeWhile((item, index) => index == 0 ? true : !IsTitle(item));
+
+                    var slide = CreateSlide(dataUntilNextSlide);
+
+                    presentation.Add(slide);
+
+                    i += dataUntilNextSlide.Count() - 1;
+                } else
+                {
+                    throw new InvalidDataException($"Line {i}: Expected Title (line starting with #). \nCannot create a new slide without a title! ");
+                }
+            }
+
+            return presentation;
+        }
+
+        private static Slide CreateSlide(IEnumerable<string> slideContents)
+        {
+            var slide = new Slide();
+
+            // Expecting first a title
+            if (slideContents.First().First() == '#')
+            {
+                slide.Title = slideContents.First().Substring(1);
+            }
+
+            // And after title the rest should be body
+            slide.Body += string.Join("\n", slideContents.Skip(1));
+
+            return slide;
+        }
+
+
+    }
+}
---
+++ PresenterTester/LoadPresentationTest.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PresenterCore;
+
+namespace PresenterTester
+{
+    [TestClass]
+    public class LoadPresentationTest
+    {
+        [TestMethod]
+        public void Existance()
+        {
+            Presentation presentation = LoadPresentation.CreatePresentationFromFileContents(new string[] { "" });
+            Assert.IsNotNull(presentation);
+        }
+
+        [TestMethod]
+        public void Title()
+        {
+            Presentation presentation = LoadPresentation.CreatePresentationFromFileContents(new string[] { "#Title" });
+            Assert.AreEqual("Title", presentation[0].Title);
+        }
+
+        [TestMethod]
+        public void MultipleTitlesOnly()
+        {
+            var contents = new string[] { "#Title1", "#Title2", "#Title3", "#some other name" };
+
+            Presentation presentation = LoadPresentation.CreatePresentationFromFileContents(contents);
+
+            Assert.AreEqual("Title1", presentation[0].Title);
+            Assert.AreEqual("Title2", presentation[1].Title);
+            Assert.AreEqual("Title3", presentation[2].Title);
+            Assert.AreEqual("some other name", presentation[3].Title);
+        }
+
+        [TestMethod]
+        public void EmptyTitle()
+        {
+            // should we allow an empty title? I guess so, it might be convinient to just have a body?
+            var contents = new string[] { "#" };
+
+            Presentation presentation = LoadPresentation.CreatePresentationFromFileContents(contents);
+
+            Assert.AreEqual("", presentation[0].Title);
+        }
+
+        [TestMethod]
+        public void NoTitle_ButThereIsBody()
+        {
+            // should we allow an empty title? I guess so, it might be convinient to just have a body?
+            var contents = new string[] { "Body" };
+            Assert.ThrowsException<InvalidDataException>(() => 
+                LoadPresentation.CreatePresentationFromFileContents(contents));
+        }
+
+        [TestMethod]
+        public void TitleAfterBody()
+        {
+            // should we allow an empty title? I guess so, it might be convinient to just have a body?
+            var contents = new string[] { "Body", "#Title" };
+            Assert.ThrowsException<InvalidDataException>(() =>
+                LoadPresentation.CreatePresentationFromFileContents(contents));
+        }
+
+        [TestMethod]
+        public void TitleAndBody()
+        {
+            var contents = new string[] { "#Title", "Body" };
+
+            Presentation presentation = LoadPresentation.CreatePresentationFromFileContents(contents);
+            Assert.AreEqual("Title", presentation[0].Title);
+            Assert.AreEqual("Body", presentation[0].Body);
+        }
+
+        [TestMethod]
+        public void TitleAndTwoLinesBody()
+        {
+            var contents = new string[] { "#Title", "Body line 1", "Body line 2" };
+
+            Presentation presentation = LoadPresentation.CreatePresentationFromFileContents(contents);
+            Assert.AreEqual("Title", presentation[0].Title);
+            Assert.AreEqual(contents[1] + "\n" + contents[2], presentation[0].Body);
+        }
+
+        [TestMethod]
+        public void TitleAndMultipleLinesBody()
+        {
+            var contents = new string[] { "#Title", "Body line 1", "Body line 2", "Body line 3" };
+
+            Presentation presentation = LoadPresentation.CreatePresentationFromFileContents(contents);
+            Assert.AreEqual("Title", presentation[0].Title);
+            Assert.AreEqual(contents[1] + "\n" + contents[2] + "\n" + contents[3], presentation[0].Body);
+        }
+
+
+        [TestMethod]
+        public void TwoSlidesWithTitleAndBody()
+        {
+            var contents = new string[] { "#Title1", "Body line 1", "Body line 2", "#Title2", "Body line 1", "Body line 2", };
+
+            Presentation presentation = LoadPresentation.CreatePresentationFromFileContents(contents);
+
+            Assert.AreEqual("Title1", presentation[0].Title);
+            Assert.AreEqual(contents[1] + "\n" + contents[2], presentation[0].Body);
+
+            Assert.AreEqual("Title2", presentation[1].Title);
+            Assert.AreEqual(contents[4] + "\n" + contents[5], presentation[1].Body);
+        }
+
+
+
+        [TestMethod]
+        public void MultipleSlidesWithTitleAndBody()
+        {
+            var contents = new string[] { "#Title1", "Body line 1", "Body line 2",
+                                          "#Title2", "Body line 1", "Body line 2",
+                                          "#Title3", "",};
+
+            Presentation presentation = LoadPresentation.CreatePresentationFromFileContents(contents);
+
+            Assert.AreEqual("Title1", presentation[0].Title);
+            Assert.AreEqual(contents[1] + "\n" + contents[2], presentation[0].Body);
+
+            Assert.AreEqual("Title2", presentation[1].Title);
+            Assert.AreEqual(contents[4] + "\n" + contents[5], presentation[1].Body);
+
+            Assert.AreEqual("Title3", presentation[2].Title);
+            Assert.AreEqual("", presentation[2].Body);
+        }
+
+
+        [TestMethod]
+        public void LoadFileWithEmptyString()
+        {
+            Assert.ThrowsException<ArgumentException>(() => LoadPresentation.LoadFromFile(""));
+        }
+
+        [TestMethod]
+        public void LoadNonExistentFile()
+        {
+            Assert.ThrowsException<FileNotFoundException>(() =>
+                LoadPresentation.LoadFromFile("this file should not exist i really hope"));
+        }
+
+    }
+}

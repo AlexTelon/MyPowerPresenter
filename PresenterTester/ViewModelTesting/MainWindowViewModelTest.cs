cc0c9c19056f4a887d8ba8b949124a39792e734b
Alex Telon <alex.telon@gmail.com>
2018-09-30 22:38:56 +0200

    MainWindowViewModel unit tests added
    Does not look pretty. Would be nice to not have to create files. But
    at the same time modifying the logic around that so that we can write
    tests that does not have to create files does not sound worth it.
    
    Skipped open file dialog tests since they open a dialog and require
    user input.

---
+++ PresenterTester/ViewModelTesting/MainWindowViewModelTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomPresenter;
+using PresenterTester.ViewModelTesting;
+using System.IO;
+
+namespace PresenterTester
+{
+    [TestClass]
+    public class MainWindowViewModelTest
+    {
+        [TestMethod]
+        public void CanLoadPresentation()
+        {
+            // Setup.
+            var mockSettings = new PresenterSettingsMock();
+            var currentDir = System.AppDomain.CurrentDomain.BaseDirectory;
+            var file = Path.Combine(currentDir, $"{nameof(MainWindowViewModelTest)}.md");
+
+            mockSettings.CurrentFile = file;
+
+            File.WriteAllText(mockSettings.CurrentFile, "#Title \nBody");
+
+            // Execute.
+            var vm = new MainWindowViewModel(mockSettings);
+
+            // Check.
+            Assert.AreEqual("Title", vm.Presentation.CurrentSlide.Title);
+            Assert.AreEqual("Body", vm.Presentation.CurrentSlide.Body);
+
+            // Teardown.
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+        }
+
+        [TestMethod]
+        public void NextSlide()
+        {
+            // Setup.
+            var mockSettings = new PresenterSettingsMock();
+            var currentDir = System.AppDomain.CurrentDomain.BaseDirectory;
+            var file = Path.Combine(currentDir, $"{nameof(MainWindowViewModelTest)}.md");
+
+            mockSettings.CurrentFile = file;
+
+            File.WriteAllText(mockSettings.CurrentFile, "#Title1 \nBody\n#Title2\nBody2");
+
+            // Execute.
+            var vm = new MainWindowViewModel(mockSettings);
+
+            vm.NextSlideCommand.Execute(null);
+
+            // Check.
+            Assert.AreEqual("Title2", vm.Presentation.CurrentSlide.Title);
+            Assert.AreEqual("Body2", vm.Presentation.CurrentSlide.Body);
+
+            // Teardown.
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+        }
+
+        [TestMethod]
+        public void PreviousSlide_WhenThereIsNone()
+        {
+            // Setup.
+            var mockSettings = new PresenterSettingsMock();
+            var currentDir = System.AppDomain.CurrentDomain.BaseDirectory;
+            var file = Path.Combine(currentDir, $"{nameof(MainWindowViewModelTest)}.md");
+
+            mockSettings.CurrentFile = file;
+
+            File.WriteAllText(mockSettings.CurrentFile, "#Title1 \nBody1\n#Title2\nBody2");
+
+            // Execute.
+            var vm = new MainWindowViewModel(mockSettings);
+
+            vm.PreviousSlideCommand.Execute(null);
+
+            // Check.
+            Assert.AreEqual("Title1", vm.Presentation.CurrentSlide.Title);
+            Assert.AreEqual("Body1", vm.Presentation.CurrentSlide.Body);
+
+            // Teardown.
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+        }
+
+
+        [TestMethod]
+        public void NextPreviousSlide()
+        {
+            // Setup.
+            var mockSettings = new PresenterSettingsMock();
+            var currentDir = System.AppDomain.CurrentDomain.BaseDirectory;
+            var file = Path.Combine(currentDir, $"{nameof(MainWindowViewModelTest)}.md");
+
+            mockSettings.CurrentFile = file;
+
+            File.WriteAllText(mockSettings.CurrentFile, "#Title1 \nBody1\n#Title2\nBody2");
+
+            // Execute.
+            var vm = new MainWindowViewModel(mockSettings);
+
+            vm.NextSlideCommand.Execute(null);
+            vm.PreviousSlideCommand.Execute(null);
+
+            // Check.
+            Assert.AreEqual("Title1", vm.Presentation.CurrentSlide.Title);
+            Assert.AreEqual("Body1", vm.Presentation.CurrentSlide.Body);
+
+            // Teardown.
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+        }
+
+    }
+}
---
+++ PresenterTester/ViewModelTesting/PresenterSettingsMock.cs
@@ -0,0 +1,22 @@
+﻿using CustomPresenter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresenterTester.ViewModelTesting
+{
+    class PresenterSettingsMock : PresenterSettings
+    {
+        public override System.Drawing.Color BackgroundColor { get; set; }
+
+        public override System.Drawing.Color Foreground { get; set; }
+
+        public override string CurrentFolder { get; set; }
+
+        public override string CurrentFile { get; set; }
+
+        public override void Save() {}
+    }
+}
